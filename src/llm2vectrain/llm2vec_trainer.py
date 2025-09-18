0778f798aa0932c883900c40299f784fd1767f27
Hans Queja <hansqueja8@gmail.com>
2025-09-18 09:33:04 +0800

    Refactor: remove comments for LLM2Vec batch processing

--- src/llm2vectrain/llm2vec_trainer.py
+++ src/llm2vectrain/llm2vec_trainer.py
@@ -10,7 +10,7 @@ def l2vec_train(l2v, lyrics):
     return vectors
 
 # For Batch Processing
-##def l2vec_train(l2v, lyrics_list):
-##    with torch.no_grad():
-##        vectors = l2v.encode(lyrics_list)  # lyrics_list: list of strings
-##    return vectors
+def l2vec_train(l2v, lyrics_list):
+    with torch.no_grad():
+        vectors = l2v.encode(lyrics_list)  # lyrics_list: list of strings
+    return vectors

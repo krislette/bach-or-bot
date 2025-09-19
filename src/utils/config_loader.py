2c16a4c7e7b72aa91afd08f49fb23bdb15a64d5f
HansQueja <hansqueja8@gmail.com>
2025-09-19 17:59:40 +0800

    Add: config_loader file to call paths as variables

---
+++ src/utils/config_loader.py
@@ -0,0 +1,20 @@
+import yaml
+from pathlib import Path
+
+# Load config
+with open("config/data_config.yml", "r") as f:
+    config = yaml.safe_load(f)
+
+BASE_DIR = Path(config["base_dir"]).resolve()
+
+# Resolve paths
+DATASET_NPZ = BASE_DIR / config["paths"]["dataset_npz"]
+DATASET_CSV = BASE_DIR / config["paths"]["dataset_csv"]
+RAW_DIR = BASE_DIR / config["paths"]["raw_dir"]
+PROCESSED_DIR = BASE_DIR / config["paths"]["processed_dir"]
+
+# Debug
+print("NPZ:", DATASET_NPZ)
+print("CSV:", DATASET_CSV)
+print("RAW_DIR:", RAW_DIR)
+print("PROCESSED_DIR:", PROCESSED_DIR)

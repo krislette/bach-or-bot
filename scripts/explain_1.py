20f5a66e4620690a3c62dee018184df4ec2d35f8
krislette <acellekrislette@gmail.com>
2025-09-30 13:16:04 +0800

    Wrapped explain_1 in a function

--- scripts/explain_1.py
+++ scripts/explain_1.py
@@ -4,24 +4,34 @@ from pathlib import Path
 from src.explainability.explainer import MusicLIMEExplainer
 from src.explainability.wrapper import MusicLIMEPredictor
 
-explainer = MusicLIMEExplainer()
-predictor = MusicLIMEPredictor()
 
-audio_path = Path("data/external/sample.mp3")
-lyrics_path = Path("data/external/sample.txt")
+def explain():
+    # Create musiclime-related instances
+    explainer = MusicLIMEExplainer()
+    predictor = MusicLIMEPredictor()
 
-y, sr = librosa.load(audio_path)
-lyrics_text = lyrics_path.read_text(encoding="utf-8")
+    # Set the path for audio and lyrics [these are samples only - song is Silver Spring]
+    audio_path = Path("data/external/sample.mp3")
+    lyrics_path = Path("data/external/sample.txt")
 
-# Fix: pass actual audio array, not shape
-explanation = explainer.explain_instance(
-    audio=y,
-    lyrics=lyrics_text,
-    predict_fn=predictor,
-    num_samples=100,  # Start small for testing
-    labels=(1,),
-)
+    # Load the audio as an object + load the lyrics as string
+    y, sr = librosa.load(audio_path)
+    lyrics_text = lyrics_path.read_text(encoding="utf-8")
 
-results = explanation.get_explanation(label=1, num_features=10)
-for item in results:
-    print(f"{item['type']}: {item['feature']} (weight: {item['weight']:.3f})")
+    # Generate explanations using musiclime
+    explanation = explainer.explain_instance(
+        audio=y,
+        lyrics=lyrics_text,
+        predict_fn=predictor,
+        num_samples=5,
+        labels=(1,),
+    )
+
+    # Print explanations
+    results = explanation.get_explanation(label=1, num_features=10)
+    for item in results:
+        print(f"{item['type']}: {item['feature']} (weight: {item['weight']:.3f})")
+
+
+if __name__ == "__main__":
+    explain()

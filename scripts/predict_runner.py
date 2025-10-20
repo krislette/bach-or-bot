0fa353441d9468f920abfc57479c80c17b92f3b6
Hans Queja <hansqueja8@gmail.com>
2025-10-20 18:04:01 +0800

    Add: predict runners for both multimodal and audio pipelines.

--- scripts/predict_runner.py
+++ scripts/predict_runner.py
@@ -1,19 +1,41 @@
 import librosa
-from scripts.predict import predict_pipeline
+from scripts.predict import predict_multimodal, predict_audio
 
-# Load test audio and lyrics
-audio_path = "data/external/sample_1.mp3"
-lyrics_path = "data/external/sample_1.txt"
 
-# Load audio
-audio_data, sr = librosa.load(audio_path)
+def predict_multimodal_runner(sample: str):
+    # Load test audio and lyrics
+    audio_path = f"data/external/{sample}.mp3"
+    lyrics_path = f"data/external/{sample}.txt"
 
-# Load lyrics
-with open(lyrics_path, "r", encoding="utf-8") as f:
-    lyrics_text = f.read()
+    # Load audio
+    audio_data, sr = librosa.load(audio_path)
 
-print("Running prediction pipeline...")
-prediction = predict_pipeline(audio_data, lyrics_text)
+    # Load lyrics
+    with open(lyrics_path, "r", encoding="utf-8") as f:
+        lyrics_text = f.read()
 
-print(f"\n=== PREDICTION RESULT ===")
-print(f"Prediction: {prediction}")
+    print("Running prediction pipeline...")
+    prediction = predict_multimodal(audio_data, lyrics_text)
+
+    print(f"\n=== PREDICTION RESULT ===")
+    print(f"Prediction: {prediction}")
+
+
+def predict_audio_runner(sample: str):
+    # Load test audio and lyrics
+    audio_path = f"data/raw/{sample}.mp3"
+
+    # Load audio
+    audio_data, sr = librosa.load(audio_path)
+
+    print("Running prediction pipeline...")
+    prediction = predict_audio(audio_data)
+
+    print(f"\n=== PREDICTION RESULT ===")
+    print(f"Prediction: {prediction}")
+
+
+if __name__ == "__main__":
+    sample = "fake_sunshine"
+
+    predict_audio_runner(sample)
